c94bfc63d4cee37e74a1b4b5d916852096fe11ae
wangfeng <wangfeng@unionpayintl.com>
2019-09-11 16:26:22 +0800

    init :springboot + bootstrap

---
+++ src/main/java/com/github/wangfeng/learnspringbootbootstrap/LearnSpringbootBootstrapApplication.java
@@ -0,0 +1,13 @@
+package com.github.wangfeng.learnspringbootbootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringbootBootstrapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnSpringbootBootstrapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/wangfeng/learnspringbootbootstrap/LearnSpringbootBootstrapApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.wangfeng.learnspringbootbootstrap;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LearnSpringbootBootstrapApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
