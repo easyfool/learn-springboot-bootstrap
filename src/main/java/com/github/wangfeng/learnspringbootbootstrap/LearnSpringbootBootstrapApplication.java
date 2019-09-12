b24fea18a2ae34ffd3d9b1935778a4a7183e1e31
wangfeng <wangfeng@unionpayintl.com>
2019-09-12 11:26:34 +0800

    stash

--- src/main/java/com/github/wangfeng/learnspringbootbootstrap/LearnSpringbootBootstrapApplication.java
+++ src/main/java/com/github/wangfeng/learnspringbootbootstrap/LearnSpringbootBootstrapApplication.java
@@ -2,8 +2,9 @@ package com.github.wangfeng.learnspringbootbootstrap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class LearnSpringbootBootstrapApplication {
 
     public static void main(String[] args) {
